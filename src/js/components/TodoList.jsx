0ed7021df763ef91dce56b78943afc3714cb6431
Meir Escobar <escobar.meir.2009@outlook.com>
2025-08-27 17:58:36 +0000

    Enter llama a handleOnClick

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,26 +1,41 @@
 import React, {useState, useEffect} from 'react';
 
 const ToDo = () => {
+
+  //usestate valor de input
   const [inputValue, setInputValue] = useState("");
+
+  //usestate valor de lista
   const [todos, setTodos] = useState([])
+
+  //funcion string de input
   const handleOnChange = (e) => {
     setInputValue(e.target.value)
   }
 
+  //Añadir el Valor de input a la lista todos y reiniciar el Valor de input.
   const handleOnClick = () => {
     setTodos([...todos, inputValue])
     setInputValue("")
   }
+
+  //Borrar el contenido de Index y actualizar todos.
   const handleDelete = (index) => {
-    const updateTodos = todos.filter((todos, i) => i !== index)
+    const updateTodos = todos.filter((tarea, i) => i !== index)
     setTodos(updateTodos)
   }
 
+  //Escucha Enter
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter"){
+      handleOnClick();
+    }
+  }
   return (
     <div className='container'>
       <h2>To do</h2>
       <div className='container-input'>
-        <input type="text" value={inputValue} onChange={handleOnChange} />
+        <input type="text" value={inputValue} onChange={handleOnChange} onKeyDown={handleKeyDown}/>
         <button className='btn-add' onClick={handleOnClick}>Add</button>
       </div>
       <ul>
@@ -35,10 +50,6 @@ const ToDo = () => {
             :
             <p>No hay tareas aún</p>
         }
-
-
-
-
       </ul>
     </div>
   )
